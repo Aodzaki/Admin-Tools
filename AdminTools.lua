a03b422e8975d9f11c8ed544316f0e1cbbffa068
Aodzaki <timcenkogleb32@gmail.com>
2024-08-17 14:13:45 +0300

    Add files via upload

--- AdminTools.lua
+++ AdminTools.lua
@@ -975,13 +975,13 @@ local pedPMy = imgui.ImFloat4( imgui.ImColor( explode_argb(HLcfg.config.pedPMy) 
 local carPMy = imgui.ImFloat4( imgui.ImColor( explode_argb(HLcfg.config.carPMy) ):GetFloat4() )  
 
 function main()
-    if not isSampfuncsLoaded() or not isSampLoaded() then
-        return
+    while not isSampAvailable() do wait(100) end
+    if autoupdate_loaded and enable_autoupdate and Update then
+        pcall(Update.check, Update.json_url, Update.prefix, Update.url)
     end
-    while not isSampAvailable() do
-        wait(100)
-    end 
-    autoupdate("https://github.com/Aodzaki/Admin-Tools/raw/main/AdminTools.json", '['..string.upper(thisScript().name)..']: ', "https://github.com/Aodzaki/Admin-Tools/raw/main/AdminTools.lua")
+    repeat
+        wait(0)
+    until sampIsLocalPlayerSpawned() 
     fixChatCoursor()
     if not doesDirectoryExist(isDirrectory) then
         createDirectory(isDirrectory)
@@ -4202,59 +4202,3 @@ function red()
     colors[clr.PlotHistogramHovered]   = ImVec4(1.00, 0.60, 0.00, 1.00)
     colors[clr.ModalWindowDarkening]   = ImVec4(0.80, 0.80, 0.80, 0.35)
 end 
-
-function autoupdate(json_url, prefix, url)
-    local dlstatus = require('moonloader').download_status
-    local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
-    if doesFileExist(json) then os.remove(json) end
-    downloadUrlToFile(json_url, json,
-      function(id, status, p1, p2)
-        if status == dlstatus.STATUSEX_ENDDOWNLOAD then
-          if doesFileExist(json) then
-            local f = io.open(json, 'r')
-            if f then
-              local info = decodeJson(f:read('*a'))
-              updatelink = info.updateurl
-              updateversion = info.latest
-              f:close()
-              os.remove(json)
-              if updateversion ~= thisScript().version then
-                lua_thread.create(function(prefix)
-                  local dlstatus = require('moonloader').download_status
-                  local color = -1
-                  sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
-                  wait(250)
-                  downloadUrlToFile(updatelink, thisScript().path,
-                    function(id3, status1, p13, p23)
-                      if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
-                        print(string.format('Загружено %d из %d.', p13, p23))
-                      elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
-                        print('Загрузка обновления завершена.')
-                        sampAddChatMessage((prefix..'Обновление завершено!'), color)
-                        goupdatestatus = true
-                        lua_thread.create(function() wait(500) thisScript():reload() end)
-                      end
-                      if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
-                        if goupdatestatus == nil then
-                          sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
-                          update = false
-                        end
-                      end
-                    end
-                  )
-                  end, prefix
-                )
-              else
-                update = false
-                print('v'..thisScript().version..': Обновление не требуется.')
-              end
-            end
-          else
-            print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
-            update = false
-          end
-        end
-      end
-    )
-    while update ~= false do wait(100) end
-end
